506b0f1f16d082f532ab3678b9629be1d464bdb5
gason <sfme@qq.com>
2012-12-15 17:45:42 +0800

    加入 初始化的测试 以及 TouchSerialPort的测试

--- Windup.SerialTalkerTest/SerialAgentTest.cs
+++ Windup.SerialTalkerTest/SerialAgentTest.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using Windup.SerialTalker;
 
 namespace Windup.SerialTalkerTest
 {
@@ -10,6 +11,31 @@ namespace Windup.SerialTalkerTest
 		{
 		}
 
+		[Test]
+		public void SerialAgentConstructTest()
+		{
+			SerialAgent s = null;
+			s = new SerialAgent();
+			Assert.IsNotNull(s);
+			s = null;
+
+			s = new SerialAgent("COM3");
+			Assert.IsNotNull(s);
+			Assert.AreEqual(s.AgentPortName, "COM3");
+			s = null;
+
+			s = new SerialAgent("COM3", 9600);
+			Assert.IsNotNull(s);
+			Assert.AreEqual(s.AgentPortName, "COM3");
+			Assert.AreEqual(s.AgentBaudRate, 9600);
+		}
+
+		[Test]
+		public void TouchAgentPortTest()
+		{
+			var result = SerialAgent.TouchAgentPort("COM3", 9600);
+			Assert.AreEqual(result, true);
+		}
 	}
 }
 
