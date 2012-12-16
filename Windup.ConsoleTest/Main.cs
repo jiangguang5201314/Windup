5e68256933ac19a57c20b482ef1813bf8cc6b966
gason <sfme@qq.com>
2012-12-16 16:06:11 +0800

    重构，测试写入的代码

--- Windup.ConsoleTest/Main.cs
+++ Windup.ConsoleTest/Main.cs
@@ -8,15 +8,23 @@ namespace Windup.ConsoleTest
 	{
 		public static void Main (string[] args)
 		{
-			Debug.WriteLine("Enter Main");
+			Debug.WriteLine ("Enter Main");
+
+			var list = SerialList.ReturnSerialList ();
+			foreach (var port in list) {
+				Console.WriteLine (port);
+			}
+
 			if (SerialAgent.TouchAgentPort ("COM3", 9600)) {
 				var s = new SerialAgent ("COM3", 9600);
-				s.AgentOpen();
-				byte[] b = new byte[]{12, 34, 56};
-				for(int i = 0; i < 100; i++){
-					Debug.WriteLine("write");
-					s.AgentWrite(b);
+				s.AgentOpen ();
+				for (int i = 0; i < 100; i++) {
+					byte[] b = new byte[]{12, 34, 56};
+					var result = s.AgentWrite (b);
+					Console.WriteLine (result);
 				}
+				s.AgentClose();
+				Debug.WriteLine("close agent");
 			}
 		}
 	}
