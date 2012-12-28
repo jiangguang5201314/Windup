d5681740078de939d6e770125015355f15c73c7a
gason <sfme@qq.com>
2012-12-29 00:39:16 +0800

    修改测试方法.

--- Windup.ConsoleTest/Main.cs
+++ Windup.ConsoleTest/Main.cs
@@ -4,28 +4,28 @@ using Windup.SerialTalker;
 
 namespace Windup.ConsoleTest
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Debug.WriteLine ("Enter Main");
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Debug.WriteLine ("Enter Main");
 
-			var list = SerialList.ReturnSerialList ();
-			foreach (var port in list) {
-				Console.WriteLine (port);
-			}
+            var list = SerialList.ReturnSerialList ();
+            foreach (var port in list) {
+                Console.WriteLine (port);
+            }
 
-			if (SerialAgent.TouchAgentPort ("COM3", 9600)) {
-				var s = new SerialAgent ("COM3", 9600);
-				s.AgentOpen ();
-				for (int i = 0; i < 100; i++) {
-					byte[] b = new byte[]{12, 34, 56};
-					var result = s.AgentWrite (b);
-					Console.WriteLine (result);
-				}
-				s.AgentClose();
-				Debug.WriteLine("close agent");
-			}
-		}
-	}
+            if (SerialAgent.TouchAgentPort ("COM3", 9600)) {
+                var s = new SerialAgent ("COM3", 9600);
+                s.AgentOpen ();
+                for (int i = 0; i < 100; i++) {
+                    byte[] b = new byte[]{12, 34, 56};
+                    var result = s.AgentWrite (b);
+                    Console.WriteLine (result);
+                }
+                s.AgentClose ();
+                Debug.WriteLine ("close agent");
+            }
+        }
+    }
 }
