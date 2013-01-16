f53814b50c0d488135871fa522f36db72d46dce5
gason <sfme@qq.com>
2013-01-17 00:07:32 +0800

    实现IAnalyzer的接口函数,空函数.

--- Windup.SerialTalker/AnalyzerWindows.cs
+++ Windup.SerialTalker/AnalyzerWindows.cs
@@ -2,9 +2,21 @@ using System;
 
 namespace Windup.SerialTalker
 {
-    public class AnalyzerWindows
+    public class AnalyzerWindows : IAnalyzer
     {
         public AnalyzerWindows ()
+        {
+        }
+
+        public void GetData (Int32 data)
+        {
+        }
+
+        public void WriteData (byte[] what)
+        {
+        }
+
+        public void GetDataFromSerialAgent (Int32 data)
         {
         }
     }
