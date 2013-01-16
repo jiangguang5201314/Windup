79fbb04932f414c1c06e1b6ccace6a53d58493af
gason <sfme@qq.com>
2013-01-17 00:06:06 +0800

    修改接口函数.

--- Windup.SerialTalker/IAnalyzer.cs
+++ Windup.SerialTalker/IAnalyzer.cs
@@ -5,5 +5,7 @@ namespace Windup.SerialTalker
     public interface IAnalyzer
     {
         void GetDataFromSerialAgent (Int32 data);
+        void GetData (Int32 data);
+        void WriteData (byte[] what);
     }
 }
