4b3f54e2c9a6367d735cba4f04014b76a2d55d6e
gason <sfme@qq.com>
2013-01-18 01:33:46 +0800

    实现了windows下判断换行符的成员函数.

--- Windup.SerialTalker/AnalyzerWindows.cs
+++ Windup.SerialTalker/AnalyzerWindows.cs
@@ -1,11 +1,40 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
     public class AnalyzerWindows : IAnalyzer
     {
+        List<Int32> tempList;
+        /// <summary>
+        /// The line break flag. if prev data is 13('\r') thus the lineBreakFlag == 1
+        /// else lineBreakFlag == 0
+        /// </summary>
+        int lineBreakFlag = 0;
+
+        bool IsLineBreak (Int32 data)
+        {
+            if (13 == data) {
+                lineBreakFlag = 1;
+                return false;
+            }
+
+            if (1 == lineBreakFlag) {
+                lineBreakFlag = 0;
+                if (10 == data) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
         public AnalyzerWindows ()
         {
+            tempList = new List<Int32> ();
         }
 
         public void GetData (Int32 data)
@@ -14,10 +43,13 @@ namespace Windup.SerialTalker
 
         public void WriteData (byte[] what)
         {
+            if (what == null)
+                throw new ArgumentNullException ("what");
         }
 
         public void TransferDelegate (Int32 data)
         {
+            tempList.Add (data);
         }
     }
 }
