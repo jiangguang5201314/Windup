ca8daf289b976034e64c8fe438840856fef039d5
gason <sfme@qq.com>
2013-01-19 01:44:54 +0800

    加入从Analyzer传出List<Int32>的delegate.

--- Windup.SerialTalker/TransferDataDelegate.cs
+++ Windup.SerialTalker/TransferDataDelegate.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
-    public delegate void TransferDataDelegate(Int32 ReceviedData);
-	public delegate void TransferDataByLinesDelegate(Int32[] ReceviedData);
+    public delegate void TransferDataDelegate (Int32 ReceviedData);
+    public delegate void TransferDataByLinesDelegate (Int32[] ReceviedData);
+    public delegate void TransferListDelegate (List<Int32> list);
 }
