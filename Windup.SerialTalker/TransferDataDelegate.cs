8c1c7516ea94699e865b46c707d787870d09113f
gason <sfme@qq.com>
2013-01-19 01:46:24 +0800

    修改形参的名称,改为小写字母.

--- Windup.SerialTalker/TransferDataDelegate.cs
+++ Windup.SerialTalker/TransferDataDelegate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
-    public delegate void TransferDataDelegate (Int32 ReceviedData);
-    public delegate void TransferDataByLinesDelegate (Int32[] ReceviedData);
+    public delegate void TransferDataDelegate (Int32 data);
+    public delegate void TransferDataByLinesDelegate (Int32[] data);
     public delegate void TransferListDelegate (List<Int32> list);
 }
