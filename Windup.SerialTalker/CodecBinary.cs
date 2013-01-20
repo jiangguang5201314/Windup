1d8c40960466656ebe3175522975c064ddfab817
gason <sfme@qq.com>
2013-01-20 21:48:43 +0800

    实现一小部分的成员函数.

--- Windup.SerialTalker/CodecBinary.cs
+++ Windup.SerialTalker/CodecBinary.cs
@@ -1,10 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
-    public class CodecBinary
+    public class CodecBinary : ICodec
     {
-        public CodecBinary ()
+        public TransferListDelegate tld;
+        public IAnalyzer analyzer;
+        public CodecBinary (IAnalyzer a)
+        {
+            analyzer = a;
+            analyzer.BindListDelegate (this.TransfListFromAnalyzer);
+        }
+
+        public void TransfListFromAnalyzer (IList<Int32> list)
+        {
+        }
+
+        public void EnCoder ()
+        {
+        }
+
+        public void DeCoder ()
         {
         }
     }
