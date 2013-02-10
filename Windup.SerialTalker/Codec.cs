ea284336b1c0641ae34d7a00b72cd9b611a0244c
gason <sfme@qq.com>
2013-02-10 17:28:43 +0800

    修改编解码器的实现.

--- Windup.SerialTalker/Codec.cs
+++ Windup.SerialTalker/Codec.cs
@@ -1,11 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
 	public class Codec
 	{
-		public Codec ()
+		IDecoder decoder;
+		IEncoder encoder;
+		IAnalyzer analyzer;
+		public Codec (ICodecFactory cf, IAnalyzer a)
 		{
+			decoder = cf.GetDecoder ();
+			encoder = cf.GetEncoder ();
+			analyzer = a;
+			analyzer.BindListDelegate (this.GetDataFromAnalyzer);
+		}
+
+		public void GetDataFromAnalyzer (IList<Int32> data)
+		{
+			decoder.Decode (data);
 		}
 	}
 }
