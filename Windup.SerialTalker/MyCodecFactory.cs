5e221f8737329a5d5fb3a0c8ba61cafb257619cd
gason <sfme@qq.com>
2013-02-10 23:11:52 +0800

    加入ICodecFactory接口的实现类.

---
+++ Windup.SerialTalker/MyCodecFactory.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Windup.SerialTalker
+{
+	public class MyCodecFactory : ICodecFactory
+	{
+		public MyCodecFactory ()
+		{
+		}
+
+		public IDecoder GetDecoder ()
+		{
+			return new MyDecoder ();
+		}
+
+		public IEncoder GetEncoder ()
+		{
+			return new MyEncoder ();
+		}
+	}
+}
+
