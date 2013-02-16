4a747447cf3de01dffd28b6b67f20fcdde2d6ae0
gason <sfme@qq.com>
2013-02-16 22:53:16 +0800

    修改解码器实现.

--- Windup.SerialTalker/MyDecoder.cs
+++ Windup.SerialTalker/MyDecoder.cs
@@ -5,14 +5,20 @@ namespace Windup.SerialTalker
 {
 	public class MyDecoder : IDecoder
 	{
-		public MyDecoder ()
+		public MyDecoder (IAnalyzer a)
 		{
+			a.BindListDelegate(this.TransferListDelegate);
 		}
 
 		public Dictionary<string, string> Decode (IList<Int32> data)
 		{
 			var d = new Dictionary<string, string> ();
 			return d;
+		}
+
+		public void TransferListDelegate(List<Int32> list)
+		{
+
 		}
 	}
 }
