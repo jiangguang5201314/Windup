bd2a01f71228cf17d76ae9e01eebac58700708b9
gason <sfme@qq.com>
2013-02-19 23:26:16 +0800

    将IAnalyzer接口替换为Analyzer抽象类.

--- Windup.SerialTalker/Codec.cs
+++ Windup.SerialTalker/Codec.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
-	public class Codec
-	{
-		IDecoder decoder;
-		IEncoder encoder;
-		IAnalyzer analyzer;
-		public Codec (ICodecFactory cf, IAnalyzer a)
-		{
-			decoder = cf.GetDecoder ();
-			encoder = cf.GetEncoder ();
-			analyzer = a;
-			analyzer.BindListDelegate (this.GetDataFromAnalyzer);
-		}
+    public class Codec
+    {
+        IDecoder decoder;
+        IEncoder encoder;
+        Analyzer analyzer;
+        public Codec (ICodecFactory cf, Analyzer a)
+        {
+            decoder = cf.GetDecoder ();
+            encoder = cf.GetEncoder ();
+            analyzer = a;
+            analyzer.BindListDelegate (this.GetDataFromAnalyzer);
+        }
 
-		public void GetDataFromAnalyzer (IList<Int32> data)
-		{
-			decoder.Decode (data);
-		}
-	}
+        public void GetDataFromAnalyzer (IList<Int32> data)
+        {
+            decoder.Decode (data);
+        }
+    }
 }
 
