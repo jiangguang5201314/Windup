d01b709caaac75fad024822bcf908c63b3e042e7
gason <sfme@qq.com>
2013-02-25 00:16:35 +0800

    加入公共的Queue,将解码后的数据保存至Dictionary中,压入到此队列中.

--- Windup.SerialTalker/Codec.cs
+++ Windup.SerialTalker/Codec.cs
@@ -3,33 +3,37 @@ using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
-    public class Codec
-    {
-        IDecoder decoder;
-        IEncoder encoder;
-        Analyzer analyzer;
-        public Codec (ICodecFactory cf, Analyzer a)
-        {
-            decoder = cf.GetDecoder ();
-            encoder = cf.GetEncoder ();
-            analyzer = a;
-            analyzer.BindListDelegate (this.GetDataFromAnalyzer);
-        }
+	public class Codec
+	{
+		IDecoder decoder;
+		IEncoder encoder;
+		Analyzer analyzer;
 
-        public void WriteData (byte[] what)
-        {
-            if (null == what)
-                throw new ArgumentNullException ("what is null");
-            analyzer.WriteData (what);
-        }
+		public Queue<Dictionary<string, string>> queue = new Queue<Dictionary<string, string>> ();
+		public Codec (ICodecFactory cf, Analyzer a)
+		{
+			decoder = cf.GetDecoder ();
+			encoder = cf.GetEncoder ();
+			analyzer = a;
+			analyzer.BindListDelegate (this.GetDataFromAnalyzer);
+		}
 
-        public void GetDataFromAnalyzer (IList<Int32> data)
-        {
-            var dic = decoder.Decode (data);
-            foreach (KeyValuePair<string, string> kvp in dic) {
-                Console.WriteLine ("key={0}, value={1}", kvp.Key, kvp.Value);
-            }
-        }
-    }
+		public void WriteData (byte[] what)
+		{
+			if (null == what)
+				throw new ArgumentNullException ("what is null");
+			analyzer.WriteData (what);
+		}
+
+		public void GetDataFromAnalyzer (IList<Int32> data)
+		{
+			var dic = decoder.Decode (data);
+			queue.Enqueue (dic);
+
+			foreach (KeyValuePair<string, string> kvp in dic) {
+				Console.WriteLine ("key={0}, value={1}", kvp.Key, kvp.Value);
+			}
+		}
+	}
 }
 
