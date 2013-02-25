4ecbb0703a6cd9c4197c6bf03a7d74f49ce9232d
gason <sfme@qq.com>
2013-02-25 17:31:59 +0800

    在Mac下实现Analyzer.

--- Windup.SerialTalker/AnalyzerMac.cs
+++ Windup.SerialTalker/AnalyzerMac.cs
@@ -2,10 +2,17 @@ using System;
 
 namespace Windup.SerialTalker
 {
-    public class AnalyzerMac
+    public class AnalyzerMac : Analyzer
     {
-        public AnalyzerMac ()
+        public AnalyzerMac (SerialAgent s) : base(s)
         {
+        }
+
+        public override bool IsLineBreak (int data)
+        {
+            bool result;
+            result = (13 == data ? true : false);
+            return result;
         }
     }
 }
