9062944ba60d4803b2d712299abdf21af3291201
gason <sfme@qq.com>
2013-02-25 13:08:19 +0800

    实现MyEncoder的Encode接口函数.

--- Windup.SerialTalker/MyEncoder.cs
+++ Windup.SerialTalker/MyEncoder.cs
@@ -2,15 +2,26 @@ using System;
 
 namespace Windup.SerialTalker
 {
-	public class MyEncoder : IEncoder
-	{
-		public MyEncoder ()
-		{
-		}
+    public class MyEncoder : IEncoder
+    {
+        public MyEncoder ()
+        {
+        }
 
-		public void Encode (byte[] what)
-		{
-		}
-	}
+        public byte[] Encode (string what)
+        {
+            if (string.IsNullOrEmpty (what))
+                throw new ArgumentNullException ("what is null or empty!");
+
+            byte[] b = {};
+            char[] c = what.ToCharArray ();
+
+            for (int i = 0; i < c.Length; i++) {
+                b [i] = byte.Parse (c [i].ToString ());
+            }
+
+            return b;
+        }
+    }
 }
 
