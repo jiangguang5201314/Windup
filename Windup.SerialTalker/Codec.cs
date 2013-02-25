37bdfd1ad1ffdf2091ce44b62059d3c485dbfd66
gason <sfme@qq.com>
2013-02-25 13:06:40 +0800

    修改Codec的WriteData成员函数的实现，通过传入字符串由encoder编码后传入分析器.

--- Windup.SerialTalker/Codec.cs
+++ Windup.SerialTalker/Codec.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 
 namespace Windup.SerialTalker
 {
-	public class Codec
-	{
-		IDecoder decoder;
-		IEncoder encoder;
-		Analyzer analyzer;
+    public class Codec
+    {
+        IDecoder decoder;
+        IEncoder encoder;
+        Analyzer analyzer;
 
-		public Queue<Dictionary<string, string>> queue = new Queue<Dictionary<string, string>> ();
-		public Codec (ICodecFactory cf, Analyzer a)
-		{
-			decoder = cf.GetDecoder ();
-			encoder = cf.GetEncoder ();
-			analyzer = a;
-			analyzer.BindListDelegate (this.GetDataFromAnalyzer);
-		}
+        public Queue<Dictionary<string, string>> queue = new Queue<Dictionary<string, string>> ();
+        public Codec (ICodecFactory cf, Analyzer a)
+        {
+            decoder = cf.GetDecoder ();
+            encoder = cf.GetEncoder ();
+            analyzer = a;
+            analyzer.BindListDelegate (this.GetDataFromAnalyzer);
+        }
 
-		public void WriteData (byte[] what)
-		{
-			if (null == what)
-				throw new ArgumentNullException ("what is null");
-			analyzer.WriteData (what);
-		}
+        public void WriteData (string what)
+        {
+            if (null == what)
+                throw new ArgumentNullException ("what is null");
+            analyzer.WriteData (encoder.Encode (what));
+        }
 
-		public void GetDataFromAnalyzer (IList<Int32> data)
-		{
-			var dic = decoder.Decode (data);
-			queue.Enqueue (dic);
+        public void GetDataFromAnalyzer (IList<Int32> data)
+        {
+            var dic = decoder.Decode (data);
+            queue.Enqueue (dic);
 
-			foreach (KeyValuePair<string, string> kvp in dic) {
-				Console.WriteLine ("key={0}, value={1}", kvp.Key, kvp.Value);
-			}
-		}
-	}
+            foreach (KeyValuePair<string, string> kvp in dic) {
+                Console.WriteLine ("key={0}, value={1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
 }
 
