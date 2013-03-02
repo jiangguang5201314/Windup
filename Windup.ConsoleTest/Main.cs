fcd1eb4eb48b33dab32ac699e3b14cfb73f51a0b
gason <sfme@qq.com>
2013-03-02 23:48:25 +0800

    修改ConsoleTest中Main函数中的实现，并加入命令行写入数据的功能.

--- Windup.ConsoleTest/Main.cs
+++ Windup.ConsoleTest/Main.cs
@@ -9,27 +9,43 @@ namespace Windup.ConsoleTest
     {
         public static void Main (string[] args)
         {
-            Debug.WriteLine ("Enter Main");
-
+            Type t = Type.GetType ("Mono.Runtime");
+            var r = t != null ? "Mono" : ".NET";
+            Console.WriteLine ("WindupConsoleTest v1.0.0");
+            Console.WriteLine ("OS: " + Environment.OSVersion.Platform);
+            Console.WriteLine ("Runtime: " + r + Environment.Version.Major);
+            /*
             var list = SerialList.ReturnSerialList ();
             foreach (var port in list) {
                 Console.WriteLine (port + "\t");
             }
+*/
 
             if (SerialAgent.TouchAgentPort ("COM3", 9600)) {
-                var s = new SerialAgent ("COM3", 9600);
+                SerialAgent s;
+                AnalyzerNT a;
+                MyCodecFactory f;
+                Codec codec;
+
+                s = new SerialAgent ("COM3", 9600);
                 s.AgentOpen ();
-                var a = new AnalyzerNT (s);
+                a = new AnalyzerNT (s);
+                f = new MyCodecFactory ();
+                codec = new Codec (f, a);
 
-                var f = new MyCodecFactory ();
-
-                var codec = new Codec (f, a);
-
-                codec.WriteData ("12345");
-                Console.Read ();
+                while (true) {
+                    Console.Write (">>>");
+                    var cmd = Console.ReadLine ();
+                    if ("EXIT" == cmd.ToUpper ()) {
+                        break;
+                    } else {
+                        codec.WriteData (cmd);
+                    }
+                }
                 s.AgentClose ();
             } else {
-                Console.WriteLine ("串口可能已被占用!");
+                Console.WriteLine ("Serial ports may be occupied.");
+                Console.WriteLine ("Press Enter exit.");
                 Console.Read ();
             }
         }
