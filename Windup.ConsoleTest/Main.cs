2b75c6a2b81b18d6cdd2ed9c7ffd88aa57574a84
gason <sfme@qq.com>
2013-04-05 13:44:04 +0800

    加入测试scriptingenginefactory的代码

--- Windup.ConsoleTest/Main.cs
+++ Windup.ConsoleTest/Main.cs
@@ -3,20 +3,30 @@ using System.Diagnostics;
 using System.Threading;
 using Windup.SerialTalker;
 using Windup.ExecuteEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Windup.ConsoleTest
 {
     class MainClass
     {
-        public static void Main (string[] args)
+        public static void Main(string[] args)
         {
+            /*
             string path = System.AppDomain.CurrentDomain.BaseDirectory;
             ScriptingEngine se = new ScriptingEngine(path + "\\scripts\\script00.py");
-            Type t = Type.GetType ("Mono.Runtime");
+            */
+            ScriptingEngineFactory factory = new ScriptingEngineFactory();
+            List<ScriptingEngine> list = factory.GetScriptingEngineList();
+            foreach (var en in list) {
+                Console.WriteLine("name: " + en.Name + "--explain: " + en.Explain);
+            }
+
+            Type t = Type.GetType("Mono.Runtime");
             var r = t != null ? "Mono" : ".NET";
-            Console.WriteLine ("WindupConsoleTest v1.0.0");
-            Console.WriteLine ("OS: " + Environment.OSVersion.Platform);
-            Console.WriteLine ("Runtime: " + r + Environment.Version.Major);
+            Console.WriteLine("WindupConsoleTest v1.0.0");
+            Console.WriteLine("OS: " + Environment.OSVersion.Platform);
+            Console.WriteLine("Runtime: " + r + Environment.Version.Major);
             /*
             var list = SerialList.ReturnSerialList ();
             foreach (var port in list) {
@@ -24,41 +34,41 @@ namespace Windup.ConsoleTest
             }
 */
 
-            if (SerialAgent.TouchAgentPort ("COM3", 9600)) {
+            if (SerialAgent.TouchAgentPort("COM3", 9600)) {
                 SerialAgent s;
                 AnalyzerNT a;
                 MyCodecFactory f;
                 Codec codec;
 
-                s = new SerialAgent ("COM3", 9600);
-                s.AgentOpen ();
-                a = new AnalyzerNT (s);
-                f = new MyCodecFactory ();
-                codec = new Codec (f, a);
+                s = new SerialAgent("COM3", 9600);
+                s.AgentOpen();
+                a = new AnalyzerNT(s);
+                f = new MyCodecFactory();
+                codec = new Codec(f, a);
 
                 while (true) {
-                    Console.Write ("> ");
-                    var cmd = Console.ReadLine ();
-                    if ("EXIT" == cmd.ToUpper ()) {
-                        Console.WriteLine ("< See you later.");
-                        Thread.Sleep (700);
+                    Console.Write("> ");
+                    var cmd = Console.ReadLine();
+                    if ("EXIT" == cmd.ToUpper()) {
+                        Console.WriteLine("< See you later.");
+                        Thread.Sleep(700);
                         break;
                     } else {
-                        codec.WriteData (cmd);
-                        Thread.Sleep (200);
+                        codec.WriteData(cmd);
+                        Thread.Sleep(200);
                     }
                     if (0 < codec.queue.Count) {
-                        var d = codec.queue.Dequeue ();
+                        var d = codec.queue.Dequeue();
                         foreach (var kv in d) {
-                            Console.WriteLine ("< " + kv.Value);
+                            Console.WriteLine("< " + kv.Value);
                         }
                     }
                 }
-                s.AgentClose ();
+                s.AgentClose();
             } else {
-                Console.WriteLine ("Serial ports may be occupied.");
-                Console.WriteLine ("Press Enter exit.");
-                Console.Read ();
+                Console.WriteLine("Serial ports may be occupied.");
+                Console.WriteLine("Press Enter exit.");
+                Console.Read();
             }
         }
     }
